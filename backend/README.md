eed35ce4945863a37554e5fddb59c445d8e6daf2
Blyat-manh <152481587+Blyat-manh@users.noreply.github.com>
2025-05-29 08:51:28 +0200

    Delete backend directory
    Signed-off-by: Blyat-manh <152481587+Blyat-manh@users.noreply.github.com>

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-# Node + Express
-
-Este proyecto es el backend de EasyServe, utiliza node con express para funcionar.
--- backend/scr/models/userModel.js
+++
@@ -1,8 +0,0 @@
-const pool = require('../utils/db');
-
-const getUserByUsername = async (username) => {
-  const [rows] = await pool.query('SELECT * FROM users WHERE name = ?', [username]);
-  return rows[0];
-};
-
-module.exports = { getUserByUsername };
